59313fedae6f3cd7702fcc8dc669a47963a0af0d
Tamas Lazar <lazartamas97@gmail.com>
2023-06-30 20:26:40 +0200

    Test commit and push

---
+++ src/test/java/com/example/stockmarketanalyzer/StockMarketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
