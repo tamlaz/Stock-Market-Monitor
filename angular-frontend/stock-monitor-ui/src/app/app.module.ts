d5d3c0c6211b6c056f1f7ec72e141f841823c6cf
Tamas Lazar <lazartamas97@gmail.com>
2023-07-29 20:20:42 +0200

    Create new components

--- angular-frontend/stock-monitor-ui/src/app/app.module.ts
+++ angular-frontend/stock-monitor-ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StockListComponent } from './components/stock-list/stock-list.component';
+import { StockDetailsComponent } from './components/stock-details/stock-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    StockListComponent,
+    StockDetailsComponent
   ],
   imports: [
     BrowserModule,
