9c151907eae9bf252e331df9a21c8aac4287a8e5
Tamas Lazar <lazartamas97@gmail.com>
2023-07-31 21:57:13 +0200

    Create methods getStockData and getStockPriceData to retrieve company details and stock price data

--- angular-frontend/stock-monitor-ui/src/app/services/stock.service.ts
+++ angular-frontend/stock-monitor-ui/src/app/services/stock.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Stock } from '../models/stock.model';
+import {StockPrice} from "../models/stock-price.model";
+import {Subject} from "rxjs";
 
 const BASE_URL = 'http://localhost:8080/api/tickers';
 
@@ -9,9 +11,21 @@ const BASE_URL = 'http://localhost:8080/api/tickers';
 })
 export class StockService {
 
+  tickerUpdate = new Subject<string>();
+
   constructor(private http: HttpClient) { }
 
   getListedStocks() {
     return this.http.get<Stock[]>(BASE_URL);
   }
+
+  getStockData(id: number | undefined) {
+    return this.http.get<Stock>(`${BASE_URL}/${id}`);
+  }
+
+  getStockPriceData(ticker: string | undefined) {
+    return this.http.get<StockPrice>(`${BASE_URL}/last/${ticker}`);
+  }
+
+
 }
