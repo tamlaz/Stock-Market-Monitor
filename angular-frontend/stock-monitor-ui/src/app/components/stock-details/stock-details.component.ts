4253a89199490a5d68c0b2662c067ae10cc2bd85
Tamas Lazar <lazartamas97@gmail.com>
2023-07-31 21:56:13 +0200

    Style and refactor stock-details component

--- angular-frontend/stock-monitor-ui/src/app/components/stock-details/stock-details.component.ts
+++ angular-frontend/stock-monitor-ui/src/app/components/stock-details/stock-details.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Stock} from "../../models/stock.model";
+import {StockService} from "../../services/stock.service";
+import {ActivatedRoute} from "@angular/router";
+import {StockPrice} from "../../models/stock-price.model";
+
 
 @Component({
   selector: 'app-stock-details',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./stock-details.component.css']
 })
 export class StockDetailsComponent {
+
+  stockId: number | undefined;
+  ticker: string | undefined;
+  stock: Stock | undefined;
+  stockPrice: StockPrice | undefined;
+
+  constructor(private stockService: StockService, private activatedRoute: ActivatedRoute) {
+      this.activatedRoute.params.subscribe(params => {
+        this.stockId = params['id'];
+      })
+  }
+
+  ngOnInit(){
+      this.stockService.getStockData(this.stockId).subscribe(response => {
+        this.stock = response;
+        console.log(response);
+      })
+  }
 
 }
