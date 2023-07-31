d765b5f265c2477674f13b23608f105c70eedca7
Tamas Lazar <lazartamas97@gmail.com>
2023-07-31 21:50:48 +0200

    Enhance stockdetails dto with price data

--- src/main/java/com/example/stockmarketanalyzer/dto/outgoing/StockDetails.java
+++ src/main/java/com/example/stockmarketanalyzer/dto/outgoing/StockDetails.java
@@ -1,6 +1,7 @@
 package com.example.stockmarketanalyzer.dto.outgoing;
 
 import com.example.stockmarketanalyzer.domain.Stock;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,18 +9,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StockDetails {
 
-
+    private Long id;
     private String ticker;
     private String name;
     private String logoUrl;
     private String iconUrl;
     private String description;
+    private double lastStockPrice;
+    private double highPrice;
+    private double lowPrice;
+    private double openPrice;
+    private double previousClosePrice;
+    private double lastTradeTime;
 
     public StockDetails(Stock stock) {
+        this.id = stock.getId();
         this.ticker = stock.getTicker();
         this.name = stock.getName();
         this.logoUrl = stock.getLogoUrl();
         this.iconUrl = stock.getIconUrl();
         this.description = stock.getDescription();
+
     }
 }
