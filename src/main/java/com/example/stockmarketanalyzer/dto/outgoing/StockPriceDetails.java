c07f81583cbbf13fd8563a6847b2c098ad6b54f4
Tamas Lazar <lazartamas97@gmail.com>
2023-08-02 13:42:40 +0200

    Refactor StockPriceDetails to only send the latest stock price to frontend

--- src/main/java/com/example/stockmarketanalyzer/dto/outgoing/StockPriceDetails.java
+++ src/main/java/com/example/stockmarketanalyzer/dto/outgoing/StockPriceDetails.java
@@ -9,20 +9,11 @@ import lombok.NoArgsConstructor;
 public class StockPriceDetails {
 
     private double lastStockPrice;
-    private double highPrice;
-    private double lowPrice;
-    private double openPrice;
-    private double previousClosePrice;
-    private double lastTradeTime;
+
 
 
     public StockPriceDetails(JsonNode jsonNode) {
         this.lastStockPrice = jsonNode.get("c").asDouble();
-        this.highPrice = jsonNode.get("h").asDouble();
-        this.lowPrice = jsonNode.get("l").asDouble();
-        this.openPrice = jsonNode.get("o").asDouble();
-        this.previousClosePrice = jsonNode.get("pc").asDouble();
-        this.lastTradeTime = jsonNode.get("t").asDouble();
     }
 
 }
