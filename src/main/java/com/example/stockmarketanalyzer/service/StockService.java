9a3f23126d1d1dde87cc50abde855da9efe5abac
Tamas Lazar <lazartamas97@gmail.com>
2023-08-02 16:45:30 +0200

    Store icon and logo relative paths in the database, save the files in the src/assets folder

--- src/main/java/com/example/stockmarketanalyzer/service/StockService.java
+++ src/main/java/com/example/stockmarketanalyzer/service/StockService.java
@@ -3,15 +3,17 @@ package com.example.stockmarketanalyzer.service;
 import com.example.stockmarketanalyzer.domain.Stock;
 import com.example.stockmarketanalyzer.dto.incoming.StockDataCommand;
 import com.example.stockmarketanalyzer.dto.outgoing.StockDetails;
-import com.example.stockmarketanalyzer.dto.outgoing.StockPriceDetails;
 import com.example.stockmarketanalyzer.repository.StockRepository;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,8 +23,12 @@ public class StockService {
 
     private StockRepository stockRepository;
     private RestTemplate restTemplate;
-    private final String FINNHUB_API_KEY = "cj1bjh9r01qhv0uhmnf0cj1bjh9r01qhv0uhmnfg";
-    private final String POLYGON_API_KEY = "bHLKcB4JTWbBZLpiv3r7_8vHjYlKmXE6";
+
+    @Value("${finnhubapikey}")
+    private String FINNHUB_API_KEY;
+
+    @Value(("${polygonapikey}"))
+    private String POLYGON_API_KEY;
 
 
     public StockService(StockRepository stockRepository, RestTemplate restTemplate) {
@@ -44,10 +50,28 @@ public class StockService {
         StockDataCommand stockDto = new StockDataCommand(jsonNode);
         String logoUrlWithApiKey = stockDto.getLogoUrl() + "?apiKey=bHLKcB4JTWbBZLpiv3r7_8vHjYlKmXE6";
         String iconUrlWithApiKey = stockDto.getIconUrl() + "?apiKey=bHLKcB4JTWbBZLpiv3r7_8vHjYlKmXE6";
-        stockDto.setLogoUrl(logoUrlWithApiKey);
-        stockDto.setIconUrl(iconUrlWithApiKey);
+        stockDto.setLogoUrl(saveImgPath(logoUrlWithApiKey,ticker));
+        stockDto.setIconUrl(saveImgPath(iconUrlWithApiKey,ticker));
         stockRepository.save(new Stock(stockDto));
         return jsonNode;
+    }
+
+    private String saveImgPath(String url, String ticker) {
+        ResponseEntity<byte[]> imageResponse = restTemplate.getForEntity(url, byte[].class);
+        byte[] imageBytes = imageResponse.getBody();
+        String relativePath =  "/assets/images/" + ticker;
+        if (url.contains("svg")) {
+            relativePath += ".svg";
+        } else {
+            relativePath += ".png";
+        }
+        String absolutePath = "/Users/lazartamas/IdeaProjects/Stock-Market-Monitor/angular-frontend/stock-monitor-ui/src" + relativePath;
+        try (FileOutputStream fos = new FileOutputStream(absolutePath)) {
+            fos.write(imageBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return relativePath;
     }
 
     public StockDetails getStockData(Long id) {
@@ -67,6 +91,6 @@ public class StockService {
     }
 
     public List<StockDetails> getAllListedStocks() {
-        return stockRepository.findAll().stream().map(StockDetails::new).collect(Collectors.toList());
+        return stockRepository.getAllStocks().stream().map(StockDetails::new).collect(Collectors.toList());
     }
 }
