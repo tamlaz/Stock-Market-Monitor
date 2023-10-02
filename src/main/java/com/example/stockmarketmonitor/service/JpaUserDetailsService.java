25e37ae8b4cec0c4b780a2b2882afbce7b4b3204
Tamas Lazar <lazartamas97@gmail.com>
2023-10-02 17:21:28 +0200

    Move register method to auth service

--- src/main/java/com/example/stockmarketmonitor/service/JpaUserDetailsService.java
+++ src/main/java/com/example/stockmarketmonitor/service/JpaUserDetailsService.java
@@ -4,6 +4,7 @@ import com.example.stockmarketmonitor.config.UserRole;
 import com.example.stockmarketmonitor.domain.CustomUser;
 import com.example.stockmarketmonitor.dto.incoming.CustomUserCommand;
 import com.example.stockmarketmonitor.repository.CustomUserRepository;
+import com.example.stockmarketmonitor.repository.UserDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -22,13 +23,11 @@ import java.util.stream.Collectors;
 @Transactional
 public class JpaUserDetailsService implements UserDetailsService {
 
-    private final CustomUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsRepository userRepository;
 
     @Autowired
-    public JpaUserDetailsService(CustomUserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public JpaUserDetailsService(UserDetailsRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -45,21 +44,5 @@ public class JpaUserDetailsService implements UserDetailsService {
                 .authorities(AuthorityUtils.createAuthorityList(roles))
                 .password(customUser.getPassword())
                 .build();
-    }
-
-
-    public void register(CustomUserCommand customUserCommand) {
-        CustomUser customUser = userRepository.findByEmail(customUserCommand.getEmail()).orElse(null);
-
-        if (customUser == null) {
-            customUser = new CustomUser();
-            customUser.setEmail(customUserCommand.getEmail());
-            customUser.setPassword(passwordEncoder.encode(customUserCommand.getPassword()));
-            customUser.setFirstName(customUserCommand.getFirstName());
-            customUser.setLastName(customUserCommand.getLastName());
-            customUser.setCreatedAt(LocalDateTime.now());
-            customUser.setUserRoles(List.of("ROLE_USER").stream().map(UserRole::valueOf).collect(Collectors.toList()));
-            userRepository.save(customUser);
-        }
     }
 }
