9324473b7f5cb44710a426210b0c6b4e1f0c1371
Tamas Lazar <lazartamas97@gmail.com>
2023-10-03 14:32:42 +0200

    Declare base url key-value pairs

---
+++ angular-frontend/stock-monitor-ui/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  AUTH_BASE_URL : 'http://localhost:8080/api/auth',
+  STOCK_BASE_URL : 'http://localhost:8080/api/tickers',
+  USER_BASE_URL : 'http://localhost:8080/api/users',
+};
---
+++ angular-frontend/stock-monitor-ui/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  AUTH_BASE_URL : 'http://localhost:8080/api/auth',
+  STOCK_BASE_URL : 'http://localhost:8080/api/stocks',
+  USER_BASE_URL : 'http://localhost:8080/api/users',
+};
