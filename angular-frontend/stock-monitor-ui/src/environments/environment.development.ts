896740d43b38d9a5673e1f679d37d7ecf204acfa
Tamas Lazar <lazartamas97@gmail.com>
2023-10-04 15:45:58 +0200

    Change stock url endpoint to /stocks

--- angular-frontend/stock-monitor-ui/src/environments/environment.development.ts
+++ angular-frontend/stock-monitor-ui/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   AUTH_BASE_URL : 'http://localhost:8080/api/auth',
-  STOCK_BASE_URL : 'http://localhost:8080/api/tickers',
+  STOCK_BASE_URL : 'http://localhost:8080/api/stocks',
   USER_BASE_URL : 'http://localhost:8080/api/users',
 };
