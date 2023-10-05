ab943af7495039fd9b9cc3ddcc0bb12472b8fad8
Tamas Lazar <lazartamas97@gmail.com>
2023-10-05 22:37:40 +0200

    Add method security to addStock method

--- src/main/java/com/example/stockmarketmonitor/controller/StockController.java
+++ src/main/java/com/example/stockmarketmonitor/controller/StockController.java
@@ -1,6 +1,5 @@
 package com.example.stockmarketmonitor.controller;
 
-import com.example.stockmarketmonitor.dto.incoming.CommonStock;
 import com.example.stockmarketmonitor.dto.outgoing.CommonStockListItem;
 import com.example.stockmarketmonitor.dto.outgoing.StockDetails;
 import com.example.stockmarketmonitor.dto.outgoing.StockListItem;
@@ -9,6 +8,7 @@ import com.example.stockmarketmonitor.service.StockService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,8 +33,9 @@ public class StockController {
     }
 
     @PostMapping("/{ticker}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<JsonNode> addStock(@PathVariable String ticker){
-        return new ResponseEntity<>(stockService.saveStock(ticker),HttpStatus.CREATED);
+        return new ResponseEntity<>(stockService.saveStock(ticker.toUpperCase()),HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
