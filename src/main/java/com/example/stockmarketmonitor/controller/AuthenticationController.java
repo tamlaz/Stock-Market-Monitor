18c88bf5e71d5d66a6e24e4f0d32ff12d4b4fc4c
Tamas Lazar <lazartamas97@gmail.com>
2023-10-10 16:13:04 +0200

    Create method to check if token is valid

--- src/main/java/com/example/stockmarketmonitor/controller/AuthenticationController.java
+++ src/main/java/com/example/stockmarketmonitor/controller/AuthenticationController.java
@@ -3,11 +3,15 @@ package com.example.stockmarketmonitor.controller;
 import com.example.stockmarketmonitor.dto.incoming.CustomUserCommand;
 import com.example.stockmarketmonitor.dto.incoming.LoginCommand;
 import com.example.stockmarketmonitor.dto.outgoing.LoginResponse;
+import com.example.stockmarketmonitor.dto.outgoing.TokenValidationResponse;
 import com.example.stockmarketmonitor.service.AuthenticationService;
+import com.example.stockmarketmonitor.service.TokenService;
 import com.example.stockmarketmonitor.validators.CustomUserCommandValidator;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +23,12 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     private final CustomUserCommandValidator userCommandValidator;
+    private final TokenService tokenService;
 
-    public AuthenticationController(AuthenticationService authenticationService, CustomUserCommandValidator userCommandValidator) {
+    public AuthenticationController(AuthenticationService authenticationService, CustomUserCommandValidator userCommandValidator, TokenService tokenService) {
         this.authenticationService = authenticationService;
         this.userCommandValidator = userCommandValidator;
+        this.tokenService = tokenService;
     }
 
     @InitBinder("customUserCommand")
@@ -41,6 +47,21 @@ public class AuthenticationController {
     @PreAuthorize("isAnonymous()")
     public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginCommand loginCommand) {
         return new ResponseEntity<>(authenticationService.loginUser(loginCommand), HttpStatus.OK);
+    }
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<TokenValidationResponse> validateToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        TokenValidationResponse validationResponse = new TokenValidationResponse();
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            Jwt accessToken = tokenService.decodeToken(authHeader.substring(7));
+            if (tokenService.isValid(accessToken)) {
+                validationResponse.setValid(true);
+                return new ResponseEntity<>(validationResponse, HttpStatus.OK);
+            }
+        }
+        validationResponse.setValid(false);
+        return new ResponseEntity<>(validationResponse, HttpStatus.UNAUTHORIZED);
     }
 
 }
