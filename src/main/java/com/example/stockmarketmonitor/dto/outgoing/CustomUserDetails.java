6c84de1f6ec5bcba57928998437ddc6906973cf5
Tamas Lazar <lazartamas97@gmail.com>
2023-10-11 12:38:13 +0200

    Add watchlist to user details

--- src/main/java/com/example/stockmarketmonitor/dto/outgoing/CustomUserDetails.java
+++ src/main/java/com/example/stockmarketmonitor/dto/outgoing/CustomUserDetails.java
@@ -5,16 +5,22 @@ import com.example.stockmarketmonitor.domain.CustomUser;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Data
 @NoArgsConstructor
 public class CustomUserDetails {
     private String firstName;
     private String lastName;
     private String email;
+    private Set<StockListItem> watchList;
 
     public CustomUserDetails(CustomUser customUser) {
         this.firstName = customUser.getFirstName();
         this.lastName = customUser.getLastName();
         this.email = customUser.getEmail();
+        this.watchList = customUser.getWatchList().stream()
+                .map(StockListItem::new).collect(Collectors.toSet());
     }
 }
