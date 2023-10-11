9ea3ff6faac54eb062e3e8a434daf400578fe7fd
Tamas Lazar <lazartamas97@gmail.com>
2023-10-11 12:39:50 +0200

    Add watchlist

--- angular-frontend/stock-monitor-ui/src/app/models/user-profile-details-model.ts
+++ angular-frontend/stock-monitor-ui/src/app/models/user-profile-details-model.ts
@@ -1,5 +1,8 @@
+import {StockListItemModel} from "./stock-list-item-model";
+
 export interface UserProfileDetailsModel {
   firstName:string;
   lastName:string;
   email:string;
+  watchList: StockListItemModel[];
 }
