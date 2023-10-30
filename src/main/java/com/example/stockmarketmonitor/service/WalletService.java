ba364c3885ff9a4e81ecb3ff0cb1fcfa7ce74da4
Tamas Lazar <lazartamas97@gmail.com>
2023-10-30 10:20:26 +0100

    Create method that handles buying stocks

--- src/main/java/com/example/stockmarketmonitor/service/WalletService.java
+++ src/main/java/com/example/stockmarketmonitor/service/WalletService.java
@@ -3,8 +3,11 @@ package com.example.stockmarketmonitor.service;
 import com.example.stockmarketmonitor.domain.CustomUser;
 import com.example.stockmarketmonitor.domain.Wallet;
 import com.example.stockmarketmonitor.repository.WalletRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 @Transactional
@@ -22,5 +25,18 @@ public class WalletService {
         Wallet walletToSave = new Wallet(1000.0, user);
         walletRepository.save(walletToSave);
         return walletToSave;
+    }
+
+    public void buyStock(Long walletId,Long stockId, Double quantity, Double price) {
+        Wallet wallet = walletRepository.findById(walletId)
+                .orElseThrow(EntityNotFoundException::new);
+        Map<Long, Double> portfolio = wallet.getPortfolio();
+        if (portfolio.containsKey(stockId)) {
+            Double updatedQuantity = portfolio.get(stockId) + quantity;
+            portfolio.put(stockId, updatedQuantity);
+        } else {
+            portfolio.put(stockId, quantity);
+        }
+        wallet.setBalance(wallet.getBalance()-price);
     }
 }
