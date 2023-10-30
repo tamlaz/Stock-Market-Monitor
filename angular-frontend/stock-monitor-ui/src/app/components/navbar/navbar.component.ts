176de57fa78a3129ca6b88545696e03f32d81c90
Tamas Lazar <lazartamas97@gmail.com>
2023-10-30 10:11:03 +0100

    Set flags for login-logout buttons

--- angular-frontend/stock-monitor-ui/src/app/components/navbar/navbar.component.ts
+++ angular-frontend/stock-monitor-ui/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AuthenticationService} from "../../services/authentication.service";
+import {AuthenticationService} from "../../services/authentication-service";
 
 @Component({
   selector: 'app-navbar',
@@ -9,6 +9,8 @@ import {AuthenticationService} from "../../services/authentication.service";
 export class NavbarComponent {
 
   isStockFormVisible = false;
+  isLoginVisible = true;
+  isLogoutVisible = false;
 
   constructor(private auth: AuthenticationService) {
     this.ngOnInit();
@@ -27,11 +29,19 @@ export class NavbarComponent {
     if (token) {
       const decodedToken = this.auth.getDecodedToken(token);
       const roles = decodedToken.roles.split(" ");
+      this.isLoginVisible = false;
+      this.isLogoutVisible = true;
       this.isStockFormVisible = roles.includes('ADMIN');
     } else {
       this.isStockFormVisible = false;
+      this.isLoginVisible = true;
+      this.isLogoutVisible = false;
     }
 
   }
 
+  logout() {
+    this.auth.logOut();
+    this.checkToken(localStorage.getItem('token'));
+  }
 }
